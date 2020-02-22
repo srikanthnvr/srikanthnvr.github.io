c36650cbb9bc65838e68d6987be79aefa8038a60
Srikanth Nangina <srikanth.n@deccansoft.net>
2020-02-22 23:57:05 +0530

    Update Files

--- mail.php
+++ mail.php
@@ -1,9 +1,9 @@
 <?php
-    $to = 'demo@spondonit.com';
-    $firstname = $_POST["fname"];
+    $to = 'demo@site.com';
+    $name = $_POST["name"];
     $email= $_POST["email"];
     $text= $_POST["message"];
-    $phone= $_POST["phone"];
+    $subject= $_POST["subject"];
     
 
 
@@ -13,17 +13,17 @@
 
     $message ='<table style="width:100%">
         <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
+            <td>'.$name.'  '.$subject.'</td>
         </tr>
         <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
+        <tr><td>phone: '.$subject.'</td></tr>
         <tr><td>Text: '.$text.'</td></tr>
         
     </table>';
 
     if (@mail($to, $email, $message, $headers))
     {
-        echo 'The message has been sent.';
+        echo 'Your message has been sent.';
     }else{
         echo 'failed';
     }
